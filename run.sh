c2bf096fbdaabfdab4a871f41e23caef10347b04
logan fisher <loganfisher@logans-MacBook-Pro.local>
2020-03-31 15:08:20 -0500

    Updated run file

--- run.sh
+++ run.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+git config --global user.email "devops@parkhub.com"
+git config --global user.name "codefresh-parkhub"
+git remote rm origin
+git remote add origin https://$GITHUB_USER:$GITHUB_TOKEN@github.com/parkhub/$REPO_NAME.git
+
 # $1 is the release type major, minor or patch
 DEBUG=release-it:* release-it --ci --no-npm
